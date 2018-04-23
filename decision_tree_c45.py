c6783cb43dc73647f0e2c44aa4d9fcf96af6a8ec
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-04-23 18:10:19 +0800

    upgrade v2

--- decision_tree_c45.py
+++ decision_tree_c45.py
@@ -26,6 +26,7 @@ class Node:
     def __init__(self, isLeaf, feature):
         self.feature = feature
         self.isLeaf = isLeaf
+        self.threshold = None
         # self.children = []
         self.children = {}
 
@@ -33,48 +34,14 @@ class C45:
     def __init__(self, file_name):
         df = pd.read_csv(file_name)
         self.feature_map = {'outlook':{'sunny':0, 'overcast':1, 'rain':2}, \
-                            'tempeature':{'low':0, 'high':1}, \
+                            'temperature':{'low':0, 'high':1}, \
                             'humidity':{'low':0, 'high':1}, \
                             'windy':{'false':0, 'true':1}, \
                             'play':{'nplay':0, 'paly':1}}
-        self.feature_list = {'outlook': 0, 'tempeature':1, 'humidity':2, 'windy':3}
-        self.train = np.array(df.astype(float).values.tolist())
-        self.En_T = self.entropy(self.train.T[4])
-        self.partitionData(self.getBestThreshold(self.train.T[2]), 2)
-        self.partitionData(self.getBestThreshold(self.train.T[1]), 1)
-        # print (self.train)
-        root_indexes = list(range(len(self.train)))
-        # root_feature = self.getMaxGainRate(self.feature_list, root_indexes)
-        self.tree = self.generateTree(self.feature_list, root_indexes)
+        self.result_key = 'play'
+        self.continuous_data_key = ['temperature', 'humidity']
+        self.tree = self.generateTree(df)
         self.printTree(self.tree)
-        # print (self.slitIndexes('outlook', root_indexes))
-
-    def getBestThreshold(self, data):
-        # 1. Sort data
-        sorted_data = np.sort(data)
-        # 2. Remove redundancey
-        delete_index = []
-        for i in range(1, len(sorted_data)):
-            if sorted_data[i] == sorted_data[i-1]:
-                delete_index.append(i)
-        sorted_data = np.delete(sorted_data, delete_index)
-        # 3. Partitioned data with H0
-        # 4. Compute all gain
-        # 5. Return H0 with max gain
-        max_gain = {'TH': 0, 'gain': 0}
-        for h0 in sorted_data:
-            partitioned_data = np.zeros(len(data))
-            for j in range(len(data)):
-                if (data[j] <= h0):
-                    partitioned_data[j] = 0.
-                else:
-                    partitioned_data[j] = 1.
-            gain = self.En_T - self.entropy(partitioned_data, self.train.T[4])
-            if (max_gain['gain'] < gain):
-                max_gain['TH'] = h0
-                max_gain['gain'] = gain
-        return max_gain['TH']
-
 
     def entropy(self, data_a, data_b=None):
         if (data_b is None):
@@ -110,21 +77,66 @@ class C45:
             # print (entropy)
             return entropy
 
-    def partitionData(self, th, index):
-        for i in range(len(self.train)):
-            if (self.train[i][index] <= th):
-                self.train[i][index] = 0
-            else:
-                self.train[i][index] = 1
+    def getBestThreshold(self, vector, result_vector):
+        # 1. Sort data
+        sorted_data = np.sort(vector)
+        # 2. Remove redundancey
+        delete_index = []
+        for i in range(1, len(sorted_data)):
+            if sorted_data[i] == sorted_data[i-1]:
+                delete_index.append(i)
+        sorted_data = np.delete(sorted_data, delete_index)
+        # 3. Partitioned data with H0
+        # 4. Compute all gain
+        # 5. Return H0 with max gain
+        max_gain = {'TH': 0, 'gain': 0}
+        for h0 in sorted_data:
+            partitioned_data = np.zeros(len(vector))
+            for j in range(len(vector)):
+                if (vector[j] <= h0):
+                    partitioned_data[j] = 0.
+                else:
+                    partitioned_data[j] = 1.
+            gain = self.entropy(result_vector) - self.entropy(partitioned_data, result_vector)
+            if (max_gain['gain'] < gain):
+                max_gain['TH'] = h0
+                max_gain['gain'] = gain
+        # print (max_gain['TH'])
+        return max_gain['TH']
 
-    def getMaxGainRate(self, features, indexes):
+    def partitionData(self, key, data, threshold):
+        for i in data.index:
+            if(data[key][i] <= threshold):
+                data[key][i] = 0.
+            else:
+                data[key][i] = 1.
+
+    def DiscreteData(self, data):
+        need_discrete = []
+        th_values = {}
+        partitioned_data = data.copy()
+        for key in partitioned_data:
+            for con_key in self.continuous_data_key:
+                if con_key == key:
+                    need_discrete.append(con_key)
+        if len(need_discrete)!=0:
+            for key in need_discrete:
+                # get max threhold
+                th = self.getBestThreshold(np.array(partitioned_data[key].tolist()), \
+                                           np.array(partitioned_data[self.result_key].tolist()))
+                # partition belong th
+                self.partitionData(key, partitioned_data, th)
+                th_values.update({key:th})
+        # print (partitioned_data)
+        return partitioned_data, th_values
+
+    def getMaxGainRate(self, data):
         max_gain = {'feature': '', 'gain': -1}
-        for key in features:
-            data_vector = []
-            for i in indexes:
-                data_vector.append(self.train.T[features[key]][i])
-            data_array = np.array(data_vector)
-            gain = self.En_T - self.entropy(data_array, self.train.T[-1])
+        result = np.array(data[self.result_key].tolist())
+        data_rm_result = data.drop(self.result_key, axis=1)
+        for key in data_rm_result:
+            data_array = np.array(data_rm_result[key].tolist())
+            gain = self.entropy(result) - self.entropy(data_array, result)
             split_gain = self.entropy(data_array)
             if split_gain == 0:
                 gain_rate = -1
@@ -133,80 +145,65 @@ class C45:
             if (max_gain['gain'] < gain_rate):
                 max_gain['gain'] = gain_rate
                 max_gain['feature'] = key
-            # print (gain)
-            # print ('key:', key, '-->', split_gain)
-            # print ('key:', key, '-->', np.array(data_vector))
-        # print ('best: ', features, indexes, max_gain['feature'])
+        # print (max_gain['feature'])
         return max_gain['feature']
 
-    def getSplitProbability(self, feature_key, data_array):
-        slit_p = np.zeros(len(self.feature_map[feature_key]))
-        index = 0
-        for key in self.feature_map[feature_key]:
-            for data in data_array:
-                if (data == self.feature_map[feature_key][key]):
-                    slit_p[index] += 1.
-            index += 1
-        # print (slit_p / len(data_array))
-        return (slit_p / len(data_array))
+    def slitData(self, best, data):
+        slit_data = {}
+        for map_key in self.feature_map[best]:
+            slit_data.update({map_key:data.copy()})
+            for index in slit_data[map_key].index:
+                # print (index)
+                if(slit_data[map_key][best][index] != self.feature_map[best][map_key]):
+                    slit_data[map_key].drop(index, inplace=True)
+            slit_data[map_key].drop(best, axis=1, inplace=True)
+        return slit_data
 
-    def generateTree(self, feature_list, indexes):
-        all_same = self.allSameResult(indexes)
-        new_feature_list = {}
-        if len(indexes) == 0:
+    def allSameResult(self, data):
+        data_list = data[self.result_key].tolist()
+        if (len(data_list) == 0):
+            return False
+        else:
+            item = data_list[0]
+            for ele in data_list:
+                if ele != item:
+                    return False
+            return item
+
+    def generateTree(self, data):
+        all_same = self.allSameResult(data)
+        if data[self.result_key].count() == 0:
             # print ('indexes = 0')
             return Node(True, None)
         elif (all_same is not False):
             # print ('all same: ', all_same)
             return Node(True, all_same)
-        elif (len(feature_list) == 0):
-            # print ('no feature_list: ')
-            return Node(True, None)
         else:
-            best = self.getMaxGainRate(feature_list, indexes)
-            # print ('recursive: ', best)
-            node = Node(False, best)
-            slit_item = self.slitIndexes(best, indexes)
-            # del new_feature_list[best]
-            for key in feature_list:
-                if key != best:
-                    new_feature_list.update({key:feature_list[key]})
+            # partition data
+            discrete_data, th_values = self.DiscreteData(data)
+            # find max gain item and slit
+            maxgain_key = self.getMaxGainRate(discrete_data)
+            # create node for each class
+            node = Node(False, maxgain_key)
+            print ('--------', maxgain_key, '--------')
+            print (data)
+            print (th_values)
+            if(maxgain_key in th_values):
+                node.threshold = th_values[maxgain_key]
+            slit_item = self.slitData(maxgain_key, data)
             for key in slit_item:
-                # print ('child ', key,': ', slit_item[key], new_feature_list)
-                node.children.update({key:self.generateTree(new_feature_list, slit_item[key])})
+                # print ('child ', key,': ', slit_item[key])
+                node.children.update({key:self.generateTree(slit_item[key])})
             return node
-
-    def allSameResult(self, indexes):
-        if (len(indexes) == 0):
-            return False
-        else:
-            item = self.train.T[-1][indexes[0]]
-            for index in indexes:
-                if (self.train.T[-1][index] != item):
-                    return False
-            return item
-
-    def slitIndexes(self, feature, indexes):
-        # print ('slit in: ', feature, indexes)
-        slit_indexes = {}
-        # slit_indexes = []
-        feature_index = self.feature_list[feature]
-        for key in self.feature_map[feature]:
-            index = []
-            for i in indexes:
-                if (self.feature_map[feature][key] == self.train.T[feature_index][i]):
-                    index.append(i)
-            slit_indexes.update({key:index})
-            # slit_indexes.append(index)
-        # print ('slit: ', slit_indexes)
-        return slit_indexes
 
     def printTree(self, tree):
         self.printNode(tree)
 
     def printNode(self, node, indent=''):
         if not node.isLeaf:
-            print (indent, node.feature, ': ')
+            print (indent, node.feature)
+            if node.threshold != None:
+                print (indent, node.threshold, ': ')
             for key in node.children:
                 print (indent+'   ', key)
                 self.printNode(node.children[key], indent+'    ')
